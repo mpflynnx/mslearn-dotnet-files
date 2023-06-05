cd8aa7137dd4026d11b79a1afeff5c061015dae9
mpflynnx <mpflynnx@gmail.com>
2023-06-05 11:33:42 +0100

    modified: Program.cs Added code to create directory salesTotalDir. Added code to create file totals.txt inside salesTotalDir.

--- Program.cs
+++ Program.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
-var salesFiles = FindFiles(storesDirectory);
+// var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+
+Directory.CreateDirectory(salesTotalDir);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
 
 IEnumerable<string> FindFiles(string folderName)
 {
