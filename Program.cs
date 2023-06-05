0bcfb81b17a55a665c33b6f6e6f71a2319029d27
mpflynnx <mpflynnx@gmail.com>
2023-06-05 11:11:24 +0100

    modified: Program.cs Updated to output all .json files

--- Program.cs
+++ Program.cs
@@ -18,7 +18,8 @@ IEnumerable<string> FindFiles(string folderName)
 
     foreach (var file in foundFiles)
     {
-        if (file.EndsWith("sales.json"))
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
         {
             salesFiles.Add(file);
         }
